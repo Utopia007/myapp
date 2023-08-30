48bb15003f10c02c2b0050c91d5e4f28b4c0429f
鹿又笑 <3062840335@qq.com>
2023-08-30 23:01:51 +0800

    鱼皮前端，进程：前后端交互

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,32 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+const Footer: React.FC = () => {
+  const defaultMessage = '鹿又笑用户中心';
+  const currentYear = new Date().getFullYear();
+  return (
+    <DefaultFooter
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: 'LuYouXiao\'s Twitter',
+          title: 'LuYouXiao\'s Twitter',
+          href: 'https://twitter.com/home',
+          blankTarget: true,
+        },
+        {
+          key: 'github',
+          title: <GithubOutlined />,
+          href: 'https://github.com/ant-design/ant-design-pro',
+          blankTarget: true,
+        },
+        {
+          key: 'Ant Design',
+          title: 'Ant Design',
+          href: 'https://ant.design',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+export default Footer;
---
+++ src/constants/index.ts
@@ -0,0 +1,11 @@
+export const SYSTEM_LOGO = "https://s2.loli.net/2023/08/28/wMRHn4kmO3C27IZ.jpg"
+
+/**
+ * 次元圈子地址
+ */
+export const CIYUAN_CIRCLE = "http://www.0e2.cn/#alipanso"
+
+/**
+ * 洛杉矶日落
+ */
+export const FORGET_CODE = "https://s2.loli.net/2023/08/30/1n3IxEWoUheuDFR.jpg"
