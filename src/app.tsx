8c6cf6e4a722e2834309ea74b387c6fb435d961e
鹿又笑 <3062840335@qq.com>
2023-09-01 22:28:08 +0800

    鱼皮前端，进程：前后端交互

--- src/app.tsx
+++ src/app.tsx
@@ -1,26 +1,31 @@
 import Footer from '@/components/Footer';
 import RightContent from '@/components/RightContent';
-import { BookOutlined, LinkOutlined } from '@ant-design/icons';
-import type { Settings as LayoutSettings } from '@ant-design/pro-components';
-import { PageLoading, SettingDrawer } from '@ant-design/pro-components';
-import type { RunTimeLayoutConfig } from 'umi';
-import { history, Link } from 'umi';
+import {BookOutlined, LinkOutlined} from '@ant-design/icons';
+import type {Settings as LayoutSettings} from '@ant-design/pro-components';
+import {PageLoading, SettingDrawer} from '@ant-design/pro-components';
+import type {RunTimeLayoutConfig} from 'umi';
+import {history, Link} from 'umi';
 import defaultSettings from '../config/defaultSettings';
-import { currentUser as queryCurrentUser } from './services/ant-design-pro/api';
+import {currentUser as queryCurrentUser} from './services/ant-design-pro/api';
+import {RequestConfig} from "@@/plugin-request/request";
 // import {RequestConfig} from "@@/plugin-request/request";
 // import {RequestConfig} from "umi";
 
 const isDev = process.env.NODE_ENV === 'development';
 const loginPath = '/user/login';
+/**
+ * 无需用户登录态的页面
+ */
+const NO_NEED_LOGIN_WHITE_LIST = ['/user/register', loginPath];
 
 /** 获取用户信息比较慢的时候会展示一个 loading */
 export const initialStateConfig = {
   loading: <PageLoading />,
 };
 
-// export const request: RequestConfig = {
-//   timeout: 10000,
-// };
+export const request: RequestConfig = {
+  timeout: 1000000,
+};
 
 /**
  * @see  https://umijs.org/zh-CN/plugins/plugin-initial-state
@@ -33,24 +38,23 @@ export async function getInitialState(): Promise<{
 }> {
   const fetchUserInfo = async () => {
     try {
-      const msg = await queryCurrentUser();
-      return msg.data;
+      return await queryCurrentUser();
     } catch (error) {
       history.push(loginPath);
     }
     return undefined;
   };
   // 如果不是登录页面，执行
-  if (history.location.pathname !== loginPath) {
-    const currentUser = await fetchUserInfo();
+  if (NO_NEED_LOGIN_WHITE_LIST.includes(history.location.pathname)) {
     return {
       fetchUserInfo,
-      currentUser,
       settings: defaultSettings,
     };
   }
+  const currentUser = await fetchUserInfo();
   return {
     fetchUserInfo,
+    currentUser,
     settings: defaultSettings,
   };
 }
@@ -60,14 +64,17 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
   return {
     rightContentRender: () => <RightContent />,
     disableContentMargin: false,
-    waterMarkProps: {
-      content: initialState?.currentUser?.name,
+    waterMarkProps: { //水印属性
+      content: initialState?.currentUser?.userName,
     },
     footerRender: () => <Footer />,
     onPageChange: () => {
       const { location } = history;
+      if (NO_NEED_LOGIN_WHITE_LIST.includes(location.pathname)){
+        return;
+      }
       // 如果没有登录，重定向到 login
-      if (!initialState?.currentUser && location.pathname !== loginPath) {
+      if (!initialState?.currentUser) {
         history.push(loginPath);
       }
     },
--- src/pages/user/Register/index.tsx
+++ src/pages/user/Register/index.tsx
@@ -1,93 +1,77 @@
 import Footer from '@/components/Footer';
-import { login } from '@/services/ant-design-pro/api';
+import {register} from '@/services/ant-design-pro/api';
 import {
   LockOutlined,
   UserOutlined,
 } from '@ant-design/icons';
 import {
   LoginForm,
-  ProFormCheckbox,
   ProFormText,
 } from '@ant-design/pro-components';
-import {Alert, Divider, message, Space, Tabs} from 'antd';
+import { message, Tabs } from 'antd';
 import React, { useState } from 'react';
-import {history, Link, useModel} from 'umi';
+import { history } from 'umi';
 import styles from './index.less';
-import {CIYUAN_CIRCLE, FORGET_CODE, SYSTEM_LOGO} from "@/constants";
-const LoginMessage: React.FC<{
-  content: string;
-}> = ({ content }) => (
-  <Alert
-    style={{
-      marginBottom: 24,
-    }}
-    message={content}
-    type="error"
-    showIcon
-  />
-);
-const Login: React.FC = () => {
-  const [userLoginState, setUserLoginState] = useState<API.LoginResult>({});
+import {CIYUAN_CIRCLE, SYSTEM_LOGO} from "@/constants";
+
+const Register: React.FC = () => {
   const [type, setType] = useState<string>('account');
-  const { initialState, setInitialState } = useModel('@@initialState');
-  const fetchUserInfo = async () => {
-    const userInfo = await initialState?.fetchUserInfo?.();
-    if (userInfo) {
-      await setInitialState((s) => ({
-        ...s,
-        currentUser: userInfo,
-      }));
+  const handleSubmit = async (values: API.RegisterParams) => {
+    const {userPassword, checkPassword} = values;
+    //校验
+    if (userPassword !== checkPassword){
+      message.error('两次密码输入不一致，请重试');
+      return;
     }
-  };
-  const handleSubmit = async (values: API.LoginParams) => {
+
     try {
-      // 登录
-      const user = await login({
-        ...values,
-        type,
-      });
-      if (user) {
-        const defaultLoginSuccessMessage = '登录成功！';
+      // 注册
+      const id = await register(values);
+
+      if (id) {
+        const defaultLoginSuccessMessage = '注册成功！';
         message.success(defaultLoginSuccessMessage);
-        await fetchUserInfo();
+
         /** 此方法会跳转到 redirect 参数所在的位置 */
         if (!history) return;
         const { query } = history.location;
         const { redirect } = query as {
           redirect: string;
         };
-        history.push(redirect || '/');
+        history.push('/user/login/redirect=' + redirect);
         return;
+      }else {
+        throw new Error(`register error id = ${id}`)
       }
-      // 如果失败去设置用户错误信息
-      setUserLoginState(user);
     } catch (error) {
-      const defaultLoginFailureMessage = '登录失败，请重试！';
+      const defaultLoginFailureMessage = '注册失败，请重试！';
       message.error(defaultLoginFailureMessage);
     }
   };
-  const { status, type: loginType } = userLoginState;
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         <LoginForm
+          submitter = {{
+            searchConfig: {
+              submitText: '注册'
+            }
+          }}
           logo={<img alt="logo" src={SYSTEM_LOGO} />}
           title="鹿又笑用户中心"
           subTitle={ <a href={CIYUAN_CIRCLE} target="_blank" rel="noreferrer"> 最好的次元圈子,点此进入 </a> }
           initialValues={{
             autoLogin: true,
           }}
+          //点击注册后，会执行里面的方法
           onFinish={async (values) => {
             await handleSubmit(values as API.LoginParams);
           }}
         >
-          <Tabs activeKey={type} onChange={setType}>
-            <Tabs.TabPane key="account" tab={'账号密码登录'} />
-          </Tabs>
 
-          {status === 'error' && loginType === 'account' && (
-            <LoginMessage content={'错误的用户名和密码'} />
-          )}
+          <Tabs activeKey={type} onChange={setType}>
+            <Tabs.TabPane key="account" tab={'账号密码注册'} />
+          </Tabs>
           {type === 'account' && (
             <>
               <ProFormText
@@ -96,7 +80,7 @@ const Login: React.FC = () => {
                   size: 'large',
                   prefix: <UserOutlined className={styles.prefixIcon} />,
                 }}
-                placeholder={'请输入账号'}
+                placeholder={'请输入注册账号'}
                 rules={[
                   {
                     required: true,
@@ -123,30 +107,33 @@ const Login: React.FC = () => {
                   },
                 ]}
               />
+              <ProFormText.Password
+                name="checkPassword"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined className={styles.prefixIcon} />,
+                }}
+                placeholder={'请再次输入密码'}
+                rules={[
+                  {
+                    required: true,
+                    message: '密码是必填项！',
+                  },
+                  {
+                    min: 8,
+                    type: "string",
+                    message: '密码长度不能小于8位！',
+                  },
+                ]}
+              />
             </>
           )}
-          {status === 'error' && loginType === 'mobile' && <LoginMessage content="验证码错误" />}
+          {/*{status === 'error' && loginType === 'mobile' && <LoginMessage content="验证码错误" />}*/}
           <div
             style={{
               marginBottom: 24,
             }}
           >
-            <Space split={<Divider type="vertical" />} align="center">
-              <ProFormCheckbox noStyle name="autoLogin">
-                自动登录
-              </ProFormCheckbox>
-              <Link to="/user/register">新用户注册</Link>
-              <a
-                style={{
-                  float: 'right',
-                }}
-                href={FORGET_CODE}
-                target="_blank"
-                rel="noreferrer"
-              >
-                忘记密码 ?
-              </a>
-            </Space>
           </div>
         </LoginForm>
       </div>
@@ -154,4 +141,4 @@ const Login: React.FC = () => {
     </div>
   );
 };
-export default Login;
+export default Register;
